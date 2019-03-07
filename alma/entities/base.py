aeb41dbe64ee92e23f4524e18768bb64e8b531ac
Olivier Lance <olivier.lance@getalma.eu>
2019-03-07 17:35:47 +0100

    Preserve original data payload in entities

--- alma/entities/base.py
+++ alma/entities/base.py
@@ -3,5 +3,7 @@ from copy import deepcopy
 
 class Base:
     def __init__(self, data):
+        self.data = data
+
         for attr, value in data.items():
             self.__setattr__(attr, deepcopy(value))
