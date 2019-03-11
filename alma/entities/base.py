69b6ff904c332787a67e330834751517bc660625
Olivier Lance <olivier.lance@getalma.eu>
2019-03-11 13:37:22 +0100

    Store entities raw data in a private `_data` attribute to prevent conflicts

--- alma/entities/base.py
+++ alma/entities/base.py
@@ -3,7 +3,11 @@ from copy import deepcopy
 
 class Base:
     def __init__(self, data):
-        self.data = data
+        self._data = data
 
         for attr, value in data.items():
             self.__setattr__(attr, deepcopy(value))
+
+    @property
+    def raw_data(self):
+        return self._data
