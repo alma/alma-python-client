4291fb24912045939c2519387ae0d3dc0662d6a0
Olivier Lance <olivier.lance@getalma.eu>
2020-09-14 09:20:45 +0200

    FeePlan entity

---
+++ alma/entities/fee_plan.py
@@ -0,0 +1,22 @@
+from enum import Enum
+
+from alma.entities import Base
+
+
+class FeePlanKind(Enum):
+    GENERAL = "general"
+    POS = "pos"
+
+
+class FeePlan(Base):
+    def __init__(self, data):
+        kind = data.pop("kind", None)
+        if kind:
+            try:
+                self.kind = FeePlanKind(kind)
+            except ValueError:
+                # Pass on unrecognized kind values
+                # restore it in the raw Payment data so that it can still be retrieved
+                data["kind"] = kind
+
+        super(FeePlan, self).__init__(data)
