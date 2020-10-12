b42b6591f8933187d2eee7a76cee5fcc994b9af1
Rémy HUBSCHER <hubscher.remy@gmail.com>
2020-10-12 17:54:56 +0200

    Improve dotted notation.
    We could also consider using https://pypi.org/project/dotted-dict/

--- alma/entities/base.py
+++ alma/entities/base.py
@@ -5,8 +5,10 @@ class Base:
     def __init__(self, data):
         self._data = data
 
-        for attr, value in data.items():
-            self.__setattr__(attr, deepcopy(value))
+    def __getattr__(self, key):
+        if not key in self._data:
+            raise AttributeError(key)
+        return self._data[key]
 
     @property
     def raw_data(self):
